129ec9371979476e6c6947d955fbaa586f2b8bea
reemz57 <157501492+reemz57@users.noreply.github.com>
2025-12-03 16:25:07 +0530

    Update validate_poi.py

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python
 
-
-"""
-    Starter code for the validation mini-project.
-    The first step toward building your POI identifier!
-
-    Start by loading/formatting the data
-
-    After that, it's not our code anymore--it's yours!
-"""
 import os
 import joblib
 import sys
-sys.path.append(os.path.abspath("../tools/"))
+
+BASE_PATH = r"C:/Users/reema/OneDrive - Indian Institute of Technology Guwahati/Documents/Udacity/ud120-projects-master/ud120-projects-master"
+sys.path.append(os.path.join(BASE_PATH, "tools"))
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = joblib.load(open("../final_project/final_project_dataset.pkl", "rb") )
+data_path = os.path.join(BASE_PATH, "final_project/final_project_dataset.pkl")
+data_dict = joblib.load(open(data_path, "rb"))
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
-
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.tree import DecisionTreeClassifier
+clf_overfit = DecisionTreeClassifier()
+clf_overfit.fit(features, labels)
+print(clf_overfit.score(features, labels))
 
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42
+)
 
-### it's all yours from here forward!  
-
-
+clf_validated = DecisionTreeClassifier()
+clf_validated.fit(features_train, labels_train)
+print(clf_validated.score(features_test, labels_test))
