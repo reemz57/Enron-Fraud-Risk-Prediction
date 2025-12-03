2827486d3916d466bbcafb6ae6bd69da67468a0d
reemz57 <157501492+reemz57@users.noreply.github.com>
2025-12-03 12:49:00 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 
-""" 
-    This is the code to accompany the Lesson 2 (SVM) mini-project.
-
-    Use a SVM to identify emails from the Enron corpus by their authors:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
 
-#########################################################
-### your code goes here ###
+clf = SVC(kernel="rbf", C=10000)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time() - t0, 3), "s")
 
-#########################################################
+t0 = time()
+pred = clf.predict(features_test)
+print("prediction time:", round(time() - t0, 3), "s")
 
-#########################################################
-'''
-You'll be Provided similar code in the Quiz
-But the Code provided in Quiz has an Indexing issue
-The Code Below solves that issue, So use this one
-'''
+accuracy = accuracy_score(labels_test, pred)
+print("accuracy:", accuracy)
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
-
-#########################################################
+print(pred[10])
+print(pred[26])
+print(pred[50])
